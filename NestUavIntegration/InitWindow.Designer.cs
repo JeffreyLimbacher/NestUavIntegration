a6ef2ebefa1bc569fa2c7ed2433be4434c36e2e8
Jeffrey Limbacher <jeffreylimbacher@gmail.com>
2015-02-14 01:37:12 -0800

    Added runtime creation of the mavlink messages. I'll add comments later.

--- NestUavIntegration/InitWindow.Designer.cs
+++ NestUavIntegration/InitWindow.Designer.cs
@@ -31,6 +31,10 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.typeSelect = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.messageLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.sendMessageButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -52,7 +56,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(197, 226);
+            this.button1.Location = new System.Drawing.Point(205, 13);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -60,11 +64,63 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // typeSelect
+            // 
+            this.typeSelect.FormattingEnabled = true;
+            this.typeSelect.Location = new System.Drawing.Point(16, 72);
+            this.typeSelect.Name = "typeSelect";
+            this.typeSelect.Size = new System.Drawing.Size(189, 21);
+            this.typeSelect.TabIndex = 3;
+            this.typeSelect.SelectedIndexChanged += new System.EventHandler(this.typeSelect_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Construct Message Manually";
+            // 
+            // messageLayout
+            // 
+            this.messageLayout.ColumnCount = 3;
+            this.messageLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.messageLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.messageLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.messageLayout.Location = new System.Drawing.Point(16, 109);
+            this.messageLayout.Name = "messageLayout";
+            this.messageLayout.RowCount = 8;
+            this.messageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.messageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.messageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.messageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.messageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.messageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.messageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.messageLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.messageLayout.Size = new System.Drawing.Size(546, 294);
+            this.messageLayout.TabIndex = 5;
+            // 
+            // sendMessageButton
+            // 
+            this.sendMessageButton.Location = new System.Drawing.Point(212, 69);
+            this.sendMessageButton.Name = "sendMessageButton";
+            this.sendMessageButton.Size = new System.Drawing.Size(117, 23);
+            this.sendMessageButton.TabIndex = 6;
+            this.sendMessageButton.Text = "Send Message";
+            this.sendMessageButton.UseVisualStyleBackColor = true;
+            this.sendMessageButton.Click += new System.EventHandler(this.sendMessageButton_Click);
+            // 
             // InitWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(691, 415);
+            this.Controls.Add(this.sendMessageButton);
+            this.Controls.Add(this.messageLayout);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.typeSelect);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
@@ -81,6 +137,10 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox typeSelect;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel messageLayout;
+        private System.Windows.Forms.Button sendMessageButton;
     }
 }
 
--- NestUavIntegration/InitWindow.cs
+++ NestUavIntegration/InitWindow.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using System.Reflection;
 using CommunicationsLayer;
 using MavLink;
 
@@ -46,12 +47,70 @@ namespace NestUavIntegration
             //Gives us the ease of user input with windows forms with the ease of output of console logging
             //super ghetto tho
             AllocConsole();
+
+            this.fillTypeSelect();
         }
 
         private void NewPacketReceived(object sender, MavlinkPacket packet)
         {
             //For now we just print out the type of message we received.
             Console.WriteLine(packet.Message.ToString());
+        }
+
+        private void fillTypeSelect()
+        {
+            Assembly mavAssem = Assembly.GetAssembly(typeof(Mavlink));
+            Type[] types = mavAssem.GetTypes();
+            typeSelect.Items.AddRange(types);
+        }
+
+        private void typeSelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Type type = (Type)typeSelect.SelectedItem;
+            FieldInfo[] fields = type.GetFields();
+            messageLayout.Controls.Clear();
+            messageLayout.RowCount = fields.Length;
+            for(int i = 0; i < fields.Length; i++)
+            {
+                FieldInfo info = fields[i];
+                TableLayoutPanelCellPosition pos = new TableLayoutPanelCellPosition();
+                pos.Row = i;
+
+                pos.Column = 0;
+                Label label = new Label();
+                label.Text = info.Name;
+                messageLayout.Controls.Add(label);
+
+                pos.Column = 1;
+                Type typeOfField = info.FieldType;
+                label = new Label();
+                label.Text = typeOfField.ToString();
+                messageLayout.Controls.Add(label);
+
+                pos.Column = 2;
+                TextBox tb = new TextBox();
+                if(typeOfField.IsValueType)
+                {
+                    tb.Text = Activator.CreateInstance(typeOfField).ToString();
+                }
+                messageLayout.Controls.Add(tb);
+            }
+            
+        }
+
+        private void sendMessageButton_Click(object sender, EventArgs e)
+        {
+            Type type = (Type)typeSelect.SelectedItem;
+            FieldInfo[] fields = type.GetFields();
+            object t = Activator.CreateInstance(type);
+            for(int i = 0; i < fields.Length; i++)
+            {
+                TextBox tb = (TextBox)messageLayout.GetControlFromPosition(2, i);
+                string val = tb.Text;
+                FieldInfo field = fields[i];
+                field.SetValue(t, Convert.ChangeType(val, field.FieldType));
+            }
+
         }
     }
 }
