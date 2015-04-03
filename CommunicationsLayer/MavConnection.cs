9184abf02631766b0c68a7fbd87f3f3f0d4ddd5a
Karanvir Panesar <singh.karanvir21@gmail.com>
2015-04-02 20:46:03 -0700

    Added Connection Interface

---
+++ CommunicationsLayer/MavConnection.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using MavLink;
+
+namespace CommunicationsLayer
+{
+    interface IMavConnection
+    {
+        enum Phase
+        {
+            NotConnected, //Haven't received anything yet.
+            Connecting, //Awaiting the param_list and heartbeats
+            NotArmed,
+            Arming,
+            Armed
+        }
+
+        Phase CurrentPhase
+        {
+            get;
+        }
+
+        int componentID
+        {
+            get;
+            set;
+        }
+        int systemID
+        {
+            get;
+            set;
+        }
+
+        //Establish a connection with the vehicle
+        void Connect();
+
+        //Arm the vehicle motors to enable movement
+        void armVehicle();
+
+        //Command the vehicle to travel to the specified waypoint
+        void goToWP();
+
+        //Return vehicle to starting position
+        void returnToLaunch();
+
+        //Send a specific message to the vehicle
+        async Task<int> SendMessage(MavlinkMessage msg);
+    }
+}
