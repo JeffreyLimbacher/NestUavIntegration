282e270be711c04408c0e49c16bcc4fb39c469b8
Jeffrey Limbacher <jeffreylimbacher@gmail.com>
2015-04-08 22:29:54 -0700

    Preliminary low level interface

---
+++ CommunicationsLayer/IComms.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationsLayer
+{
+    class IComms
+    {
+
+        bool Connected { get; }
+
+        /// <summary>
+        /// This opens the connection to the specified point. The class should start off connected in
+        /// both the serial port case and the udp port case, so the call to this should only be called
+        /// if the connection point got disconnected.
+        /// </summary>
+        Task<bool> Connect();
+
+        /// <summary>
+        /// Send the specified bytes starting at point offset up until offset+length (exclusive)
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="offset"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        Task<int> SendAsync(byte[] data, int offset, int length);
+
+        /// <summary>
+        /// Receive the bytes from the connection.
+        /// </summary>
+        /// <returns></returns>
+        Task<byte[]> ReceiveAsync();
+
+        /// <summary>
+        /// Closes the connection with the specified point.
+        /// </summary>
+        void Close();
+
+    }
+}
